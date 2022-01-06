f006393dec922ee9d2483f5e5af3e1041a713b10
Script47 <Script47@hotmail.com>
2022-01-06 19:58:34 +0000

    Update PhpInstallService.php

--- src/Services/PhpInstallService.php
+++ src/Services/PhpInstallService.php
@@ -4,20 +4,26 @@ namespace Getevm\Evm\Services;
 
 use Getevm\Evm\Abstracts\InstallServiceAbstract;
 use Getevm\Evm\Interfaces\InstallServiceInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\Console\Command\Command;
 
 class PhpInstallService extends InstallServiceAbstract implements InstallServiceInterface
 {
     /**
      * @return int
+     * @throws GuzzleException
      */
     public function execute()
     {
         $this->getOutput()->writeln('Requesting PHP v' . $this->getConfig()['version']);
 
-        $response = $this->getGuzzle()->get($this->getUnixRelease());
+        $response = $this->getGuzzle()->get($this->getUnixRelease(), [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream'
+            ]
+        ]);
 
-        if ($response->getStatusCode() === 404) {
+        if ($response->getStatusCode() === 400) {
             $this->getOutput()->writeln('PHP v' . $this->getConfig()['version'] . ' cannot be found.');
 
             return Command::INVALID;
