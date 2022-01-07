da812040618c65c07898cc2f37b423e73dfeab7c
Script47 <Script47@hotmail.com>
2022-01-07 19:58:55 +0000

    Update setpath.bat

--- bin/setpath.bat
+++ bin/setpath.bat
@@ -1,26 +1,12 @@
 @echo off
 
 SetLocal EnableDelayedExpansion
-
 set KeyName=Path
 set OldInstallDir=%1
 set NewInstallDir=%2
-
-if "%OldInstallDir%" == "" (
-    echo Old installation directory missing. Exiting.
-) else (
-    if "%NewInstallDir%" == "" (
-        echo New installation directory missing. Exiting.
-    ) else (
-        set OldPath=%Path%
-        set NewPath=!OldPath:%OldInstallDir%=!
-
-        echo "%NewPath%"
-
-        ::setx /m "%KeyName%" "%NewPath%";
-
-        pause
-    )
-)
-
+set OldPath=%Path%
+set NewPath=!OldPath:%OldInstallDir%=!
+echo "%NewPath%"
+::setx /m "%KeyName%" "%NewPath%";
+pause
 EndLocal
