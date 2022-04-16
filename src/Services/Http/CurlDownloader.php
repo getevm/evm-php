f85ab0d34c7d111bfe02bbc5975d7d957898582c
Script47 <Script47@hotmail.com>
2022-04-16 03:24:53 +0100

    Update CurlDownloader.php

--- src/Services/Http/CurlDownloader.php
+++ src/Services/Http/CurlDownloader.php
@@ -9,7 +9,6 @@ class CurlDownloader
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, $progressFunction);
         curl_setopt($ch, CURLOPT_NOPROGRESS, false);
