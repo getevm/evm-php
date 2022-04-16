f15e8688a4a638919a633cb54b82fb70c0257fbe
Script47 <Script47@hotmail.com>
2022-04-16 03:23:19 +0100

    CurlDownloader test

---
+++ src/Services/Http/CurlDownloader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Getevm\Evm\Services\Http;
+
+class CurlDownloader
+{
+    public function downloadZip(string $url, callable $progressFunction, callable $errorFunction)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, $progressFunction);
+        curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+}
