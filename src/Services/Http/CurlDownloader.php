f1f75283be2480dd3490ada74f94e190181c5ee3
Script47 <Script47@hotmail.com>
2022-04-17 01:51:34 +0100

    Bug fix

--- src/Services/Http/CurlDownloader.php
+++ src/Services/Http/CurlDownloader.php
@@ -4,24 +4,25 @@ namespace Getevm\Evm\Services\Http;
 
 class CurlDownloader
 {
-    private $progressFunction = null;
-
-    public function download(string $url)
+    public function download(string $url, callable $progressFunction = null)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, [$this, 'progressFunction']);
-        curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+
+        if (is_callable($progressFunction)) {
+            function progress($resource, $downloadTotal, $downloaded, $uploadTotal, $uploaded) {
+                $progressFunction($resource, $downloadTotal, $downloaded, $uploadTotal, $uploaded);
+            }
+
+            curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'progress');
+            curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+        }
+
         $response = curl_exec($ch);
         curl_close($ch);
 
         return $response;
-    }
-
-    public function setProgressFunction(callable $func)
-    {
-        $this->progressFunction = $func;
     }
 }
