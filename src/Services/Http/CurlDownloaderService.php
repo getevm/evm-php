1e6562f5d11c58197ca699a67eb2dfeb554c76ae
Script47 <Script47@hotmail.com>
2022-04-17 02:45:18 +0100

    Update CurlDownloaderService.php

--- src/Services/Http/CurlDownloaderService.php
+++ src/Services/Http/CurlDownloaderService.php
@@ -26,7 +26,7 @@ class CurlDownloaderService
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36 RuxitSynthetic/1.0 v1208824127884685839 t8068951106021062059');
         $response = curl_exec($ch);
 
         if ($response === false) {
