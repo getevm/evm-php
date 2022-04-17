0c8c6ff4c67909acaf8a80297bd8bb77f3ef00ad
Script47 <Script47@hotmail.com>
2022-04-17 01:26:24 +0100

    Bug fix

--- src/Services/Http/CurlDownloader.php
+++ src/Services/Http/CurlDownloader.php
@@ -4,12 +4,12 @@ namespace Getevm\Evm\Services\Http;
 
 class CurlDownloader
 {
-    public function downloadZip(string $url, callable $progressFunction, callable $errorFunction)
+    public function download(string $url, callable $progressFunction, callable $errorFunction)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
         curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, $progressFunction);
         curl_setopt($ch, CURLOPT_NOPROGRESS, false);
         $response = curl_exec($ch);
