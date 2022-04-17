0225867969821fdba5e53a0d0b9d3f7469c53fca
Script47 <Script47@hotmail.com>
2022-04-17 01:54:26 +0100

    Bug fix

--- src/Services/Http/CurlDownloader.php
+++ src/Services/Http/CurlDownloader.php
@@ -4,26 +4,24 @@ namespace Getevm\Evm\Services\Http;
 
 class CurlDownloader
 {
-    public function download(string $url, callable $progressFunction = null)
+    public function download(string $url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-
-        if (is_callable($progressFunction)) {
-            function progress($resource, $downloadTotal, $downloaded, $uploadTotal, $uploaded) {
-                global $progressFunction;
-                $progressFunction($resource, $downloadTotal, $downloaded, $uploadTotal, $uploaded);
-            }
-
-            curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'progress');
-            curl_setopt($ch, CURLOPT_NOPROGRESS, false);
-        }
-
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, [$this, 'progressFunction']);
+        curl_setopt($ch, CURLOPT_NOPROGRESS, false);
         $response = curl_exec($ch);
         curl_close($ch);
 
         return $response;
+    }
+
+    public function progressFunction($resource, $downloadTotal, $downloaded, $uploadTotal, $uploaded)
+    {
+        if ($downloadTotal > 0) {
+            echo $downloaded / $downloadTotal * 100 . '%' . PHP_EOL;
+        }
     }
 }
