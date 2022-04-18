765f2e5ad9542c6847d531a48d9e40d259d7073a
Script47 <Script47@hotmail.com>
2022-04-18 16:29:14 +0100

    Update InstallService.php

--- src/Services/Php/InstallService.php
+++ src/Services/Php/InstallService.php
@@ -30,7 +30,7 @@ class InstallService extends InstallServiceAbstract implements InstallServiceInt
         if (!file_exists(self::PATH_TO_PHP_METADATA)) {
             $this->getConsoleOutputService()->warning('Version file missing. Attempting to synchronise with https://getevm.github.io/versions/php.json');
 
-            $syncService = new SyncService($this->getOutputInterface();
+            $syncService = new SyncService($this->getOutputInterface());
 
             echo json_encode([
                     $syncService->execute()
